c60efd124a23118fe507d72fa1b12c71bfe993ac
Lê Quang Bảo <lequangbao@gmail.com>
2016-12-05 20:49:08 +0700

    test
    just a test, i didn't do anything yet

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//test
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -15,3 +16,4 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+
